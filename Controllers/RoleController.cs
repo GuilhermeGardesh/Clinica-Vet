4413075677f9be90cf73f2e878470e9d7f603af3
GuilhermeGardesh <guil33310@gmail.com>
2023-06-12 21:36:17 -0300

    feat: adicionando gerenciamento de sessões

---
+++ Controllers/RoleController.cs
@@ -0,0 +1,94 @@
+﻿using ClinicaVet.GestaoVeterinaria.Models.Roles;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations;
+
+namespace ClinicaVet.GestaoVeterinaria.Controllers
+{
+    public class RoleController : Controller
+    {
+        private readonly RoleManager<IdentityRole> _roleManager;
+        private readonly UserManager<IdentityUser> _userManager;
+
+        public RoleController(RoleManager<IdentityRole> roleManager, UserManager<IdentityUser> userManager)
+        {
+            _roleManager = roleManager;
+            _userManager = userManager;
+        }
+        public IActionResult Index()
+        {
+            return View(_roleManager.Roles);
+        }
+
+        public IActionResult Create() => View();
+
+        [HttpPost]
+        public async Task<IActionResult> Create([Required] string name)
+        {
+            if (ModelState.IsValid)
+            {
+                await _roleManager.CreateAsync(new IdentityRole(name));
+                return RedirectToAction("Index");
+            }
+            return View(name);
+        }
+
+        public IActionResult Delete() => View();
+
+        [HttpPost]
+        public async Task<IActionResult> Delete(string id)
+        {
+            IdentityRole role = await _roleManager.FindByIdAsync(id);
+            if (role != null)
+            {
+                await _roleManager.DeleteAsync(role);
+                return RedirectToAction("Index");
+            }
+            ModelState.AddModelError("", "No role found");
+            return View("Index", _roleManager.Roles);
+        }
+
+        public async Task<IActionResult> Update(string id)
+        {
+            var role = await _roleManager.FindByIdAsync(id);
+            var members = new List<IdentityUser>();
+            var nonMembers = new List<IdentityUser>();
+            foreach (var user in _userManager.Users)
+            {
+                var list = await _userManager.IsInRoleAsync(user, role.Name) ? members : nonMembers;
+                list.Add(user);
+            }
+            return View(new RoleEdit
+            {
+                Role = role,
+                Members = members,
+                NonMembers = nonMembers
+            });
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Update(RoleModification model)
+        {
+            if (ModelState.IsValid)
+            {
+                foreach (string userId in model.AddIds ?? new string[] { })
+                {
+                    var user = await _userManager.FindByIdAsync(userId);
+                    if (user != null)
+                        await _userManager.AddToRoleAsync(user, model.RoleName);
+                }
+                foreach (string userId in model.DeleteIds ?? new string[] { })
+                {
+                    var user = await _userManager.FindByIdAsync(userId);
+                    if (user != null)
+                        await _userManager.RemoveFromRoleAsync(user, model.RoleName);
+                }
+            }
+
+            if (ModelState.IsValid)
+                return RedirectToAction(nameof(Index));
+            else
+                return await Update(model.RoleId);
+        }
+    }
+}
